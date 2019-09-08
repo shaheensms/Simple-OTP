b63f2feba9cda05ea49eac9a5f8920ec5d74005f
shaheensms <shaheensms111@gmail.com>
2019-09-08 19:48:21 +0600

    Initial commit

---
+++ app/src/main/java/com/example/simpleotp/CountryData.java
@@ -0,0 +1,7 @@
+package com.example.simpleotp;
+
+public class CountryData {
+    public static final String[] countryNames = {"Bangladesh", "India", "USA"};
+    public static final String[] countryAreaCode = {"880", "99", "11"};
+}
+
---
+++ app/src/main/java/com/example/simpleotp/ProfileActivity.java
@@ -0,0 +1,29 @@
+package com.example.simpleotp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        findViewById(R.id.buttonLogOut).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+    }
+}
